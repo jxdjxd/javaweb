b9b6d4487edfbd05677dbaed876fbab8b6ff1414
jxd <333333@qq.com>
2021-09-19 13:26:38 +0800

    Change Teacher class

--- git_test/src/main/java/com/jxd/bean/Teacher.java
+++ git_test/src/main/java/com/jxd/bean/Teacher.java
@@ -5,4 +5,13 @@ package com.jxd.bean;
  * @date 2021/9/19 13:22
  */
 public class Teacher {
+    private String name;
+
+    public Teacher(){
+
+    }
+
+    public Teacher(String name){
+        this.name = name;
+    }
 }
