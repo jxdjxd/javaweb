f66a371a0144d04b53d98537314ee2405b7b5e0f
jxd <333333@qq.com>
2021-09-19 13:42:41 +0800

    test Student class

---
+++ git_test/src/test/java/com/jxd/bean/TestStudent.java
@@ -0,0 +1,15 @@
+package com.jxd.bean;
+
+import org.junit.Test;
+
+/**
+ * 测试Student类
+ * @author jxd
+ * @date 2021/9/19 13:37
+ */
+public class TestStudent {
+    @Test
+    public void test(){
+
+    }
+}
